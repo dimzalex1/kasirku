b6071bc58ace97c4de1d50ecbb18c55f31491c2d
dimzalex1 <dimzalex123@gmail.com>
2026-05-17 19:27:01 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,12 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: true,
+  },
+
   plugins: [
     react(),
     mode === "development" && componentTagger(),
